2d4393106861cbfff87c15e7133d227379e4b29b
antonarhipov <ant.arhipov@gmail.com>
2024-05-17 14:23:16 +0300

    Add User class and expand ApplicationTest
    Added a Serializable User data class to the Application.kt file, which will enable the serialization and deserialization of User objects. In ApplicationTest.kt, increased the coverage of tests to handle new User class endpoints. All endpoints for CRUD operations are prepared for testing.

--- src/main/kotlin/jetbrains/org/Application.kt
+++ src/main/kotlin/jetbrains/org/Application.kt
@@ -4,6 +4,7 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import jetbrains.org.plugins.*
+import kotlinx.serialization.Serializable
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -13,4 +14,18 @@ fun main() {
 fun Application.module() {
     configureSerialization()
     configureRouting()
+}
+
+@Serializable
+data class User(
+    val userId: Int,
+    val userType: UserType,
+    val displayName: String,
+    val link: String,
+    val aboutMe: String? = null
+)
+
+enum class UserType {
+    REGISTERED,
+    MODERATOR,
 }
--- src/test/kotlin/jetbrains/org/ApplicationTest.kt
+++ src/test/kotlin/jetbrains/org/ApplicationTest.kt
@@ -5,17 +5,52 @@ import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
 import jetbrains.org.plugins.*
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.Json
 import kotlin.test.*
 
 class ApplicationTest {
-    @Test
-    fun testRoot() = testApplication {
+
+    var fakeData = mutableListOf<User>(
+        User(1, UserType.REGISTERED, "Anton", "x.com/antonarhipov", aboutMe = "I speak Kotlin"),
+        User(2, UserType.REGISTERED, "Leonid", "x.com/___e5l", aboutMe = "I make Ktor"),
+        User(3, UserType.REGISTERED, "John Doe", "en-wp.org/wiki/John_Doe", aboutMe = "I am John Doe"),
+    )
+
+    private val testApp = TestApplication {
         application {
-            configureRouting()
-        }
-        client.get("/").apply {
-            assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
+            module()
         }
     }
+
+    private val client = testApp.createClient {}
+
+    @Test
+    fun `test root endpoint`(): Unit = runBlocking {
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello, World!", bodyAsText())
+        }
+    }
+
+    @Test
+    fun `get all data`(): Unit = runBlocking {
+
+    }
+
+    @Test
+    fun `post data instance`(): Unit = runBlocking {
+
+    }
+
+    @Test
+    fun `put data instance`(): Unit = runBlocking {
+
+    }
+
+    @Test
+    fun `delete data instance`(): Unit = runBlocking {
+        
+    }
+
 }
