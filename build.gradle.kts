68b7c1723ac118157a19f5d3349b2de3b4445373
antonarhipov <ant.arhipov@gmail.com>
2024-05-17 14:33:28 +0300

    Implement tests. No implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-client-content-negotiation-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
--- src/test/kotlin/jetbrains/org/ApplicationTest.kt
+++ src/test/kotlin/jetbrains/org/ApplicationTest.kt
@@ -3,6 +3,7 @@ package jetbrains.org
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.testing.*
 import jetbrains.org.plugins.*
 import kotlinx.coroutines.runBlocking
@@ -11,11 +12,7 @@ import kotlin.test.*
 
 class ApplicationTest {
 
-    var fakeData = mutableListOf<User>(
-        User(1, UserType.REGISTERED, "Anton", "x.com/antonarhipov", aboutMe = "I speak Kotlin"),
-        User(2, UserType.REGISTERED, "Leonid", "x.com/___e5l", aboutMe = "I make Ktor"),
-        User(3, UserType.REGISTERED, "John Doe", "en-wp.org/wiki/John_Doe", aboutMe = "I am John Doe"),
-    )
+    var fakeData = mutableListOf<User>()
 
     private val testApp = TestApplication {
         application {
@@ -23,7 +20,20 @@ class ApplicationTest {
         }
     }
 
-    private val client = testApp.createClient {}
+    private val client = testApp.createClient {
+        install(io.ktor.client.plugins.contentnegotiation.ContentNegotiation) {
+            json()
+        }
+    }
+
+    @BeforeTest
+    fun initializeState(){
+        fakeData = mutableListOf(
+            User(1, UserType.REGISTERED, "Anton", "x.com/antonarhipov", aboutMe = "I speak Kotlin"),
+            User(2, UserType.REGISTERED, "Leonid", "x.com/___e5l", aboutMe = "I make Ktor"),
+            User(3, UserType.REGISTERED, "John Doe", "en-wp.org/wiki/John_Doe", aboutMe = "I am John Doe"),
+        )
+    }
 
     @Test
     fun `test root endpoint`(): Unit = runBlocking {
@@ -35,22 +45,47 @@ class ApplicationTest {
 
     @Test
     fun `get all data`(): Unit = runBlocking {
-
+        client.get("/data").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            val data = Json.decodeFromString<List<User>>(bodyAsText())
+            assertEquals(fakeData.size, data.size)
+        }
     }
 
     @Test
     fun `post data instance`(): Unit = runBlocking {
-
+        val response = client.post("/data") {
+            contentType(ContentType.Application.Json)
+            setBody(User(123, UserType.REGISTERED, "A", "a.com", aboutMe = "AAA"))
+        }
+        assertEquals(HttpStatusCode.Created, response.status)
+        assertEquals("Data added successfully", response.bodyAsText())
     }
 
     @Test
     fun `put data instance`(): Unit = runBlocking {
-
+        val updatedDataResponse = client.put("/data") {
+            contentType(ContentType.Application.Json)
+            val user = fakeData.first()
+            setBody(user.copy(displayName = "Mr. ${user.displayName}"))
+        }
+        assertEquals(HttpStatusCode.OK, updatedDataResponse.status)
+        assertEquals("Data updated successfully", updatedDataResponse.bodyAsText())
     }
 
     @Test
     fun `delete data instance`(): Unit = runBlocking {
-        
+        client.delete("/data/1").apply {
+            // Assertions to confirm the successful deletion of the Data instance
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Data deleted successfully", bodyAsText())
+        }
+
+        client.get("/data/1").apply {
+            // Assertions to confirm the successful fetching of the updated Data instances
+            assertEquals(HttpStatusCode.NotFound, status)
+        }
     }
+
 
 }
